3981cd8d24fb16209be5c480f1efc9216b2bb866
niokle <myszapl@gmail.com>
2020-11-12 14:18:24 +0100

    spring boot main class added

---
+++ src/main/java/com/klenio/rsstoemail/RssToEmailApplication.java
@@ -0,0 +1,12 @@
+package com.klenio.rsstoemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssToEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RssToEmailApplication.class, args);
+    }
+}
